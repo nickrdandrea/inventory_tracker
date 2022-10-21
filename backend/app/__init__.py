be5abcde42c16788abf5978dc205ed6fce6ae68a
nickrdandrea <nickrdandrea@gmail.com>
2022-10-21 15:09:08 -0400

    Add Flask-Migrate to utilize Alembic

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_restful import Api
 
 from config import Config
@@ -8,15 +9,17 @@ from app.resources import ItemResource, LoginResource, RegisterResource, VendorR
 
 
 login = LoginManager()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
+    migrate.init_app(app, db)
     db.init_app(app)
-    db.create_all(app=app)
-    populate_db(app)
+    # db.create_all(app=app)
+    # populate_db(app)
     login.init_app(app)
 
     api = Api(app)
