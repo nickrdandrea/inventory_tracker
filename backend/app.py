b165fda004030ca267b10e85245cb0c03c5d3234
nickrdandrea <nickrdandrea@gmail.com>
2022-10-21 15:08:27 -0400

    Add Flask Shell context

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,17 @@
-from app import create_app
+from app import create_app, db
+from app.models import Alert, Item, User, Vendor
 
 app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+                'db': db, 
+                'Alert': Alert, 
+                'Item': Item, 
+                'User': User, 
+                'Vendor': Vendor
+            }
 
 if __name__ == "__main__":
     app.run()
