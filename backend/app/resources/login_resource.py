65ba3d538ccb72ecd032909b79ca94abc0f66e39
Nick D'Andrea <nickrdandrea@gmail.com>
2023-01-23 15:47:11 -0500

    fix login resource to handle basic auth

--- backend/app/resources/login_resource.py
+++ backend/app/resources/login_resource.py
@@ -1,26 +1,30 @@
 from flask_jwt_extended import create_access_token
 from flask_restful import Resource, reqparse
+from base64 import b64decode
 
 from app.models.user import User
-
 
 class LoginResource(Resource):
     def __init__(self) -> None:
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('username', type = str, required = True,
-            help = 'No username provided.')
-        self.reqparse.add_argument('password', type = str, required = True,
-            help = 'No password provided.')
+        self.reqparse.add_argument('Authorization', location='headers', required= True)
         super(LoginResource, self).__init__()
 
     def post(self):
         args = self.reqparse.parse_args()
-        username = args['username']
-        password = args['password']
-
+        auth_type, encoded_credentials = args['Authorization'].split()
+        
+        if auth_type.lower() == 'basic':
+            credentials = b64decode(encoded_credentials).decode().split(':')
+        else:
+            return {"message": "Invalid authentication type."}, 401
+        
+        username = credentials[0]
+        password = credentials[1]
+                
         user = User.query.filter_by(name=username).first()
         if not user or not user.check_password(password):
             return {"message": "Wrong username or password."}, 401
-
+        
         access_token = create_access_token(identity=user)
         return {"access_token": access_token}, 200
