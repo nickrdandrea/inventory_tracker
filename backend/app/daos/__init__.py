a212191014751347396a83ffebc4cec30dfbf680
Nick D'Andrea <nickrdandrea@gmail.com>
2023-01-27 13:37:40 -0500

    remove daos and references

--- backend/app/daos/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.daos.core_dao import CoreDao
-from app.daos.item_dao import ItemDao
-from app.daos.vendor_dao import VendorDao
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from app import create_app, db
 from app.models import Alert, Item, User, Vendor
 from app.updater.updater import updater
-from shelltools import test_data, test_upsert
 from app.db_utils import add_vendor, drop_records, search_item
 
 app = create_app()
@@ -13,8 +12,6 @@ def make_shell_context():
             "Item": Item, 
             "User": User, 
             "Vendor": Vendor,
-            "test_data": test_data,
-            "test_upsert": test_upsert,
             "add_vendor": add_vendor,
             "drop_records": drop_records,
             "search_item": search_item,
